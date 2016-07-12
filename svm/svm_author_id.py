4d17ccb38c8f447339e468d1fcab136d3ef4fcdf
nicolasrannou <npm.rannou@gmail.com>
2016-07-12 11:09:51 +0200

    SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,37 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
-#########################################################
-### your code goes here ###
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100]
 
 #########################################################
+def SVM():
+  
+    from sklearn.svm import SVC
+    clf = SVC( kernel="rbf", C=10000.0 )
 
+    t0 = time()
+    clf.fit( features_train, labels_train )
+    print "fitting time:", round(time()-t0, 3), "s"
+
+    t0 = time()
+    prediction = clf.predict( features_test )
+    print "prediction time:", round(time()-t0, 3), "s"
+    print "prediction 10:", prediction[10]
+    print "prediction 26:", prediction[26]
+    print "prediction 50:", prediction[50]
+
+    chris = 0
+    for user in prediction:
+        if user == 1:
+            chris += 1
+    
+    print "predictions for chris", chris
+       
+    from sklearn.metrics import accuracy_score
+    accuracy = accuracy_score( prediction, labels_test )
+    print "accuracy:", accuracy 
+#########################################################
+
+SVM()
 
